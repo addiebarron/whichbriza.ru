d38a7af1120981859ebb53365c4fcd2baf75be48
addiebarron.os <addie.barron@outschool.com>
2022-06-22 09:40:56 -0400

    Convert to static site

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-// webpack
-import './sass/style.scss';
-
 window.$ = document.querySelector.bind(document);
 window.$$ = document.querySelectorAll.bind(document);
 
@@ -9,57 +6,57 @@ let resultsElem, btnElem, imgElem, textElem;
 // image hrefs
 let images = [
   {
-    src: 'briza1',
-    name: 'Musing Briza',
-    alt: 'Briza looking upwards.',
+    src: "briza1",
+    name: "Musing Briza",
+    alt: "Briza looking upwards.",
     text: `u turn ur gaze the stars and comtemplate the bigger questions... r there cubbies on other planets? does someone out there have more hair than me? u do not know, and u are ok with that.`,
   },
   {
-    src: 'briza2',
-    name: 'Sus Briza',
-    alt: 'alt text 2',
+    src: "briza2",
+    name: "Sus Briza",
+    alt: "alt text 2",
     text: `here's the thing. u got schemes. u got plans, machinations, big stinky skeletons in the closet. u are sus.`,
   },
   {
-    src: 'briza3',
-    name: 'Gay and Beautiful Briza',
-    alt: 'alt text 3',
+    src: "briza3",
+    name: "Gay and Beautiful Briza",
+    alt: "alt text 3",
     text: `u are gay and beautiful briza :)`,
   },
   {
-    src: 'briza4',
-    name: 'BABY',
-    alt: 'alt text4',
-    text: 'U ARE BABY. U. BABY. U R. BABY!!!!',
+    src: "briza4",
+    name: "BABY",
+    alt: "alt text4",
+    text: "U ARE BABY. U. BABY. U R. BABY!!!!",
   },
   {
-    src: 'briza5',
-    name: 'Nutso Briza',
-    alt: 'alt text5',
+    src: "briza5",
+    name: "Nutso Briza",
+    alt: "alt text5",
     text: `u are off the WALL my dude! completely zoinked my guy! sniffing left and right like some kind of demon. absolutely incredible. never stop.`,
   },
   {
-    src: 'briza6',
-    name: 'Flat Briza',
-    alt: 'alt text6',
+    src: "briza6",
+    name: "Flat Briza",
+    alt: "alt text6",
     text: `the weight of the world is too much. here u shall stay... squished into this couch cushion... overwhelmed by ur great burdens of responsibility. ur gonna be ok, babe.`,
   },
 ];
 
-document.addEventListener('DOMContentLoaded', () => {
-  resultsElem = $('#results-container');
-  btnElem = $('#btn-container button');
-  imgElem = $('#img-container img');
-  textElem = $('#results-text');
+document.addEventListener("DOMContentLoaded", () => {
+  resultsElem = $("#results-container");
+  btnElem = $("#btn-container button");
+  imgElem = $("#img-container img");
+  textElem = $("#results-text");
 
-  preloadImages(images.map(image => `img/${image.src}.jpg`));
+  preloadImages(images.map((image) => `img/${image.src}.jpg`));
 
   btnElem.onclick = whichBriza;
 });
 
-const whichBriza = async e => {
-  $('#btn-container').classList.add('hidden');
-  $('#results-container').classList.remove('hidden');
+const whichBriza = async (e) => {
+  $("#btn-container").classList.add("hidden");
+  $("#results-container").classList.remove("hidden");
 
   const decision = await randomSrc();
 
@@ -86,8 +83,8 @@ const randomSrc = async () => {
   return currentImage;
 };
 
-const renderResults = image => {
-  resultsElem.classList.add('finished');
+const renderResults = (image) => {
+  resultsElem.classList.add("finished");
   textElem.innerHTML = `<h1>${image.name}</h1><p>${image.text}</p>`;
 };
 
@@ -111,4 +108,4 @@ function preloadImages(array) {
   }
 }
 
-const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
+const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
